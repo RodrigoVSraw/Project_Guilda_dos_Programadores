feb02790e964fd4c57a6f8d0212278580f797910
Rodrigo Raw <rodrigo.rraw@gmail.com>
2026-05-26 16:39:08 -0300

    Configurações iniciais

---
+++ Project_guildaAPI/Models/Aventureiro.cs
@@ -0,0 +1,14 @@
+﻿using GuildaM.Models;
+
+namespace Aventureiro.Models
+{
+    public abstract class Aventureiro
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Nivel { get; set; }
+        public float Vida { get; set; }
+        public Guilda Guilda { get; set; }  
+
+    }
+}
---
+++ Project_guildaAPI/Models/Guilda.cs
@@ -0,0 +1,10 @@
+﻿namespace Guilda.Models
+{
+    public class Guilda
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Nivel { get; set; }  
+        public int NivelRequerido { get; set; }
+    }
+}
